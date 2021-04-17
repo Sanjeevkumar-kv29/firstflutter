f5a043f7c077cbf59a2e5260b0c3ba03c5a58775
SANJEEV KUMAR <62838848+sanjeev-divine@users.noreply.github.com>
2021-04-17 12:05:46 +0530

    initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,38 @@
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  //WidgetApp MaterialApp CupertinoApp
+  runApp(MaterialApp(
+    home: Homepaget2(),
+    theme: ThemeData(primarySwatch: Colors.grey),
+    /*home: Container(
+      color: Colors.teal,
+      child: Text('lolo flutter'),
+    ),*/
+  ));
+}
+
+class Homepaget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Text('tatti'),
+    );
+  }
+}
+
+class Homepaget2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Scaffold give appbar to us so we use it for appbar
+      appBar: AppBar(
+        title: Text('tattiflutter'),
+      ),
+      body: Container(
+        child: Text('TattiXD'),
+      ),
+    );
+  }
 }
