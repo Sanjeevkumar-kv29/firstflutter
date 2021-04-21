0aa7290d71e5515a9ab25513ebc575f8d2c66b19
SANJEEV KUMAR <62838848+sanjeev-divine@users.noreply.github.com>
2021-04-21 15:34:13 +0530

    in this branch we organised our code into some folder and flies ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  //WidgetApp MaterialApp CupertinoApp
-  runApp(MaterialApp(
-    home: Homepaget2(),
-    theme: ThemeData(primarySwatch: Colors.grey),
+import 'Activities/HomeActivity.dart';
 
-    /*home: Container(
-      color: Colors.teal,
-      child: Text('lolo flutter'),
-    ),*/
-  ));
+void main() {
+
+  //WidgetApp MaterialApp CupertinoApp
+  runApp(Myapp());
+
 }
 
 class Homepaget1 extends StatelessWidget {
@@ -25,96 +19,19 @@ class Homepaget1 extends StatelessWidget {
   }
 }
 
-// converted statefull widget coz we want to change some data dynamicaally in text feild
-class Homepaget2 extends StatefulWidget {
-  @override
-  _Homepaget2State createState() => _Homepaget2State();
-}
-
-class _Homepaget2State extends State<Homepaget2> {
-  var displaytext = 'I love you dude';
-  TextEditingController txtcntroler = TextEditingController();
-
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Scaffold give appbar to us.
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text('hello flutter'),
-      ),
+    return MaterialApp(
 
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: SingleChildScrollView(
-            child: Card(
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/img1.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 20),
-                  Text(displaytext, //'hola lola',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: TextField(
-                      controller: txtcntroler,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Text feild',
-                          labelText: 'textfeild'),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home:Homeactivity(), //from activity folder we rendering activity page here
 
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: [
-            /*DrawerHeader(
-              child: Text('hello'),
-              decoration: BoxDecoration(color: Colors.grey),
-            ),*/
-            UserAccountsDrawerHeader(
-              accountName: Text('name'),
-              accountEmail: Text('email@email.email'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://netstorage-legit.akamaized.net/images/vllkyt1uc3jd8n61v8.jpg'),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('niceflutter'),
-              subtitle: Text('hola hola hola'),
-              onTap: () {},
-            )
-          ],
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.grey),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          displaytext = txtcntroler.text;
-
-          setState(() {});
-        },
-        backgroundColor: Colors.black,
-        child: Icon(
-          Icons.send,
-          color: Colors.white,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      /*home: Container(
+      color: Colors.teal,
+      child: Text('lolo flutter'),
+    ),*/
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:firstflutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Homepaget2());
+    await tester.pumpWidget(Myapp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
